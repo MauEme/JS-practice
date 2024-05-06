207a503b8171812a111e7f0c8a026560f252b5ce
Mau <mauricioJroja@gmail.com>
2024-05-06 09:28:30 -0300

    clase jjavascript 1version 1

---
+++ JS/script.js
@@ -0,0 +1,43 @@
+let alumno = "fede";
+let nota = 9;
+
+console.log("el nombre del alumno es: " + alumno + " y su nota es: " + nota);
+
+// STRING
+let str1 = "Juan"
+let str2 = "Pedo"
+let str3 = "Ramiro"
+
+let str4 = str1 + " y " +str2;
+
+console.log(str4)
+
+/* // NUMBER
+let num1 = 2
+let num2 = 3
+
+// Operaciones basicas
+let suma = num1 + num2
+let resta = num1 - num2
+let multiplicacion = num1 * num2
+console.log("La suma es: " + suma) // CONSOLE para trabajar en consola (de esta manera se trabaja, desde el browser)
+console.log("La resta es: " + resta)
+console.log("La multiplicacion es: " + multiplicacion) */
+
+// INGRESO DE DATOS DEL USUARIO
+let num1 = parseInt(prompt("Ingrese el primer numero"))
+let num2 = parseInt(prompt("Ingrese el segundo numero"))
+
+let suma = num1 + num2
+let resta = num1 - num2
+let multiplicacion = num1 * num2
+
+alert("La suma es: " + suma) // ALERT para que nos salga el pop-up <>
+alert("La resta es: " + resta)
+alert("La multiplicacion es: " + multiplicacion)
+
+document.write("<h1>Javascript<h1/>" + mult) // DOCUMENT WRITE escribe en el documento
+
+
+
+
